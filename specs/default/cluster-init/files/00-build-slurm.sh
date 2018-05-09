ca227f38e8121cd6707066fc79eb71401fab84cf
Ben Hummerstone <b.hummerstone@outlook.com>
2018-05-09 16:00:37 +0100

    Update 00-build-slurm.sh
    Change wget command to use correct variable (SLURM_PKG)

--- specs/default/cluster-init/files/00-build-slurm.sh
+++ specs/default/cluster-init/files/00-build-slurm.sh
@@ -4,7 +4,7 @@ SLURM_PKG="slurm-17.11.0.tar.bz2"
 DOWNLOAD_URL="https://download.schedmd.com/slurm"
 
 yum install -y rpm-build munge-devel munge-libs readline-devel openssl-devel pam-devel perl-ExtUtils-MakeMaker gcc mysql mysql-devel
-wget "${DOWNLOAD_URL}/${SLURM_URL}"
+wget "${DOWNLOAD_URL}/${SLURM_PKG}"
 
 rpmbuild -ta ${SLURM_PKG}
 
